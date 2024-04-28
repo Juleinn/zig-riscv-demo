7a715499d4e4e6bc93ba20cdb1bcd4c9e9010a92
Juleinn <julientartem@hotmail.fr>
2024-04-28 18:41:48 +0200

    Add basic integer printing utility

--- build.zig
+++ build.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const optimize = b.standardOptimizeOption(.{});
-
     const exe = b.addExecutable(.{
         .name = "firmware",
         .root_source_file = b.path("src/main.zig"),
         .target = b.resolveTargetQuery(.{ .cpu_arch = .riscv64, .os_tag = .freestanding }),
-        .optimize = optimize,
+        // change this to .Debug if needed but will generate a huge binary (1.8MB hello-world).
+        // change the RAM region size in qemu.ld accordingly if needed
+        .optimize = .ReleaseSmall,
         .code_model = .medium,
     });
 
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,31 @@
+fn puti(value: i32) void {
+    if (value == 0) {
+        putc('0');
+        return;
+    }
+    var current_value: i32 = value;
+    if (value < 0) {
+        putc('-');
+        current_value = -value;
+    }
+    var buffer: [64]u8 = undefined;
+    var digit_count: u8 = 0;
+    while (current_value != 0) {
+        const current_digit = @mod(current_value, 10);
+        current_value = @divTrunc((current_value - current_digit), 10);
+        const current_char: u8 = @intCast(current_digit + '0');
+        buffer[digit_count] = current_char;
+        digit_count += 1;
+    }
+
+    while (digit_count != 0) : (digit_count -= 1) {
+        putc(buffer[digit_count - 1]);
+    }
+}
+
 fn putc(ch: u8) void {
-    const ptr: *u8 = @ptrFromInt(0x10000000);
+    // needs be volatile otherwise repeated calls get optimized out
+    const ptr: *volatile u8 = @ptrFromInt(0x10000000);
     ptr.* = ch;
 }
 
@@ -21,5 +47,8 @@ fn test_inline() i32 {
 
 export fn main() void {
     puts("Hello, world");
+    puti(42);
+    puts("\r\n");
+    puti(-42);
     while (true) {}
 }
